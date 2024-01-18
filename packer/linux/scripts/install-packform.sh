d637cbb82ffb32d36f03361f5a4eca9fc769ce8e
Wildan Maulana Syahidillah <wildan2711@gmail.com>
2024-01-18 08:39:58 +0700

    FST2 - Ansible is updated to latest versions (#1)

--- packer/linux/scripts/install-packform.sh
+++ packer/linux/scripts/install-packform.sh
@@ -2,13 +2,13 @@
 set -eu -o pipefail
 
 echo "Creating Ansible venv..."
-sudo /usr/local/bin/python3.10 -m venv /srv/venv/ansible
+sudo python3.11 -m venv /srv/venv/ansible
 sudo chmod -R +rw /srv/venv/ansible
 sudo chown -R ec2-user: /srv/venv/ansible
 
 echo "Priming venv..."
 source /srv/venv/ansible/bin/activate
-pip3 install ansible==7.2.0 boto3 docker requests
+pip3 install ansible==9.1.0 boto3 docker==6.0.1 requests
 deactivate
 
 # TODO: pre-install Docker build images
